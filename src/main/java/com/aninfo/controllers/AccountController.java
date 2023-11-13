6d25d7fe9aeacc9476b6d1a5f809de961859ea34
gonzashima <gonza.shima@gmail.com>
2023-11-12 23:52:49 -0300

    imports

--- src/main/java/com/aninfo/controllers/AccountController.java
+++ src/main/java/com/aninfo/controllers/AccountController.java
@@ -1,10 +1,8 @@
 package com.aninfo.controllers;
 
-import com.aninfo.Memo1BankApp;
 import com.aninfo.model.Account;
 import com.aninfo.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
